08a645509c7ced5ce0d91b5d91ee04e5a4f3432f
khaled07-ag <khaled-kk01@hotmail.com>
2024-08-18 20:49:06 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import prducts from './data';
 function App() {
+ 
+  const productList = prducts.map((product)=>{
+          
+    return (
+      <div className= 'item-container'>
+    <img height={300} width={200} src={product.image} alt="product"/>
+    <h3>{product.title}</h3>
+    <p>{product.price}</p>
+  </div>
+  )})
   return (
-    <div className="App">
+    
+    
+        <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        <h1>MAMA'S BAKERY</h1>
+        <p>Freshly made</p>
+        </div>
+        <div>
+          <img className="image" src="https://img.freepik.com/free-vector/outside-bakery-shop-background_1308-122413.jpg" alt="Bakery"/>
+        </div>
+        <div className='item-section'>
+        {/* <div className= 'item-container'>
+          <img height={300} width={200} src={prducts[0].image} alt="croissant"/>
+          <h3>{prducts[0].title}</h3>
+          <p>{prducts[0].price}</p>
+        </div>
+        <div className='item-container'>
+          <img  height={300} width={200}  src={prducts[1].image} alt="cookie"/>
+        
+        <h3>{prducts[1].title}</h3>
+        <p>{prducts[1].price}</p>
+        </div> */}
+        {/* {prducts.map((product)=>{
+          
+          return (
+            <div className= 'item-container'>
+          <img height={300} width={200} src={product.image} alt="product"/>
+          <h3>{product.title}</h3>
+          <p>{product.price}</p>
+        </div>
+        )})
+        } */}
+         {productList}
+         
+        </div>
       </header>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,15 @@
+import croissant from "./cro.jpg";
+let prducts =[
+    {
+      title:"Plain Croissant",
+      image: croissant,
+      price: "10KD"
+    },
+    {
+      title:"Chocolate Chip Cookies",
+      image: "https://static01.nyt.com/images/2022/02/12/dining/JT-Chocolate-Chip-Cookies/JT-Chocolate-Chip-Cookies-square640.jpg",
+      price: "5KD"
+    }
+    
+  ]
+  export default prducts;
